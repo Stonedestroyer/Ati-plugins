c844f04d19e2a811b07b945212c06540f208adfd
Stylite <https://Stylite@github.com>
2018-03-22 09:51:48 -0400

    Caramba -> v3

--- caramba/caramba.py
+++ caramba/caramba.py
@@ -1,14 +1,9 @@
+import discord
+
 class Caramba:
     def __init__(self, bot):
         self.bot = bot
-
-    async def listener(self, message):
-        if message.author.id != self.bot.user.id:
-            if message.content.lower().startswith('ayy') or message.content.lower().startswith('aayy'):
-                await self.bot.send_message(message.channel, '¡Caramba!')
-
-
-def setup(bot):
-    n = Caramba(bot)
-    bot.add_listener(n.listener, "on_message")
-    bot.add_cog(n)
+        
+    async def on_message(self, message):
+        if message.content.lower().startswith('ayy') or message.content.lower().startswith('aayy'):
+            await ctx.send("¡Caramba!")
